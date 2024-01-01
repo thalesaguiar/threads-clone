88874ff7b7d1062f3c81001364653641559ce499
thalesaguiar <dev.thalesaguiar@gmail.com>
2024-01-01 01:07:09 -0300

    feat: added animated refresh component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,38 @@
-import { SafeAreaView, ScrollView } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+} from "react-native";
+import Lottie from "lottie-react-native";
+import { setBackgroundColorAsync } from "expo-system-ui";
+import { RefreshControl } from "react-native-gesture-handler";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
 export default function TabOneScreen() {
   return (
     <SafeAreaView>
-      <ScrollView>
-
+      <StatusBar barStyle={"dark-content"} translucent />
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 40 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => {}}
+            tintColor={"transparent"}
+          />
+        }
+      >
+        <Lottie
+          source={require("../../assets/Animation - 1704071784133.json")}
+          loop={false}
+          autoPlay
+          style={{ width: 90, height: 90, alignSelf: "center" }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
